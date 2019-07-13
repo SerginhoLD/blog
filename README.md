7225eca51484cd3ba47a8ef147490ed62410ef81
Sergey Zubrilin <serginhold.box@gmail.com>
2019-07-13 23:40:41 +0300

    slim

--- README.md
+++ README.md
@@ -5,9 +5,8 @@
 ## Установка
 
 1) git, composer
-2) `docker build -f docker/Dockerfile -t serginhold/blog-phalcon:latest .` \
-`docker-compose up -d` \
-`docker exec -i -t blog-phalcon bash -c "cd /var/www; bash"`
-3) `phalcon migration run`
-4) создать единственного admin-пользователя \
-`php bin/cli user {login} {password}`
+2) `docker build -f docker/Dockerfile -t serginhold/blog:latest .` \
+`docker-compose up -d`
+
+## Console
+`docker exec -i -t blog bash -c "cd /var/www; bash"`
--- html/index.php
+++ html/index.php
@@ -1,23 +1,2 @@
 <?php
-error_reporting(E_ALL);
-
-use Phalcon\DI;
-use Phalcon\Mvc\Application;
-
-try
-{
-    $config = require __DIR__ . '/../config/config.php';
-
-    require_once $config->projectDir . '/config/loader.php';
-
-    $di = new DI\FactoryDefault();
-    require $config->projectDir . '/config/services.php';
-
-    $application = new Application($di);
-    $response = $application->handle();
-    $response->send();
-}
-catch (\Exception $e)
-{
-    var_dump($e);
-}
+echo 'slim';
