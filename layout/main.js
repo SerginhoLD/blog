e635a29208c98656cd2574130b9fd1148a9df179
Sergey Zubrilin <serginhold.box@gmail.com>
2019-07-24 15:23:07 +0300

    highlightjs langs

--- layout/main.js
+++ layout/main.js
@@ -1,7 +1,16 @@
 import './scss/main.scss';
 import 'highlight.js/styles/default.css';
 import 'highlight.js/styles/color-brewer.css';
-import hljs from 'highlight.js';
+
+import hljs from 'highlight.js/lib/highlight';
+import hljsPhp from 'highlight.js/lib/languages/php';
+import hljsJs from 'highlight.js/lib/languages/javascript';
+import hljsCss from 'highlight.js/lib/languages/css';
+import hljsXml from 'highlight.js/lib/languages/xml';
+hljs.registerLanguage('php', hljsPhp);
+hljs.registerLanguage('javascript', hljsJs);
+hljs.registerLanguage('css', hljsCss);
+hljs.registerLanguage('xml', hljsXml);
 
 //import Vue from './node_modules/vue/dist/vue.js';
 //import hello from './components/hello.vue';
